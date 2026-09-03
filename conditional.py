96ec01a46b036420158a2de5c5a399834b629a20
Elias <elias.arellano.campos@gmail.com>
2026-09-03 15:00:45 -0500

    Add ex

--- conditional.py
+++ conditional.py
@@ -1,3 +1,4 @@
+import stat
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 
@@ -10,3 +11,17 @@ class AgentState(TypedDict):
 def adder(state : AgentState) -> AgentState:
     """This node adds 2 nums"""
     return {'finalNum' : state['num1'] + state['num2']}
+
+def substraction(state : AgentState) -> AgentState:
+    """This node adds 2 nums"""
+    return {'finalNum' : state['num1'] - state['num2']}
+
+def decide_next_node(state : AgentState) -> AgentState:
+    """This node will select the next phase of the graph"""
+
+    if state['operation'] == "+":
+        return "add_operation"
+    elif state['operation'] == "-":
+        return "sub_operation"
+
+    
