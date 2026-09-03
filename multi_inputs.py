0a9161079a3c9f4bcc5c9c1680e92f5dbe3b5869
Elias <elias.arellano.campos@gmail.com>
2026-09-03 11:35:07 -0500

    Add ex

--- multi_inputs.py
+++ multi_inputs.py
@@ -1,13 +1,22 @@
+from socket import gaierror
 import stat
 from typing import TypedDict, List
 from langgraph.graph import StateGraph
 
-class AgentState(TypedDict):
+class AgentState(TypedDict): # Create a state
     values : List[int]
     name : str
     result : str
 
-def process_values(state : AgentState) -> AgentState:
+def process_values(state : AgentState) -> AgentState: # Create a node
     """ This pocess hanldes multiple different inputs """
-    
+
     return {'result': f"Hi there {state['name']}! Your sum = {sum(state['values'])}"}
+
+graph = StateGraph(AgentState) # Start a graph
+
+graph.add_node("processor", process_values) # Add a node
+graph.set_entry_point("processor") # Add start
+graph.set_finish_point("processor") # Add end
+
+app = graph.compile() # Compile graph 
