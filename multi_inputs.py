38fe438821b6fff27df5cb4fbb64f097da9314f9
Elias <elias.arellano.campos@gmail.com>
2026-09-03 11:44:41 -0500

    Add ex

--- multi_inputs.py
+++ multi_inputs.py
@@ -10,7 +10,7 @@ class AgentState(TypedDict): # Create a state
 
 def process_values(state : AgentState) -> AgentState: # Create a node
     """ This pocess hanldes multiple different inputs """
-
+    print(state)
     return {'result': f"Hi there {state['name']}! Your sum = {sum(state['values'])}"}
 
 graph = StateGraph(AgentState) # Start a graph
@@ -20,3 +20,7 @@ graph.set_entry_point("processor") # Add start
 graph.set_finish_point("processor") # Add end
 
 app = graph.compile() # Compile graph 
+
+answers = app.invoke({'values' : [1,2,3,4], 'name' : 'Sancho'})
+
+print(answers['result'])
