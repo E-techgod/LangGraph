a4880dc49adcf8885781f344dba9afb37c10777c
Elias <elias.arellano.campos@gmail.com>
2026-09-07 17:58:33 -0500

    Agent2: Add memory

--- agent2.py
+++ agent2.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from typing import TypedDict, List, Union
 from langgraph.graph import StateGraph, START, END
-from langchain_core.messages import HumanMessage, AIMessage 
+from langchain_core.messages import HumanMessage, AIMessage
 
 load_dotenv() # Function to load the key
 
@@ -20,7 +20,22 @@ def process_node(state : AgentState) -> AgentState: # Create the first node
     """ This code will solve the request you input"""
     response = llm.invoke(state['messages']) # contains both messages, either AI or Human
 
-    state['messages'].append(AIMessage(content= response.content)) # .content retieves only the important part of the answer, not the whole process
+    state['messages'].append(AIMessage(content = response.content)) # .content retieves only the important part of the answer, not the whole process
     print(f"\nAI: {response.content}")
 
     return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process_node)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+conversation_history = [] # This is where we'll store the conversation 
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Enter: ")
