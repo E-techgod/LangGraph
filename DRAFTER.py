4c1d58cf4168b2f9335647c9b9a8523088ac1646
Elias <elias.arellano.campos@gmail.com>
2026-09-08 12:28:17 -0500

    AI Agentics System that speed up drafting documents, emails, among other things

--- DRAFTER.py
+++ DRAFTER.py
@@ -7,7 +7,7 @@ from langgraph.graph import StateGraph, END
 from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph 
 from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and tool_call_id
 from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM 
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage
 from langgraph.graph.message import add_messages # Allows to append all messages to the state without overriding any of it 
 from typing import TypedDict, Annotated, Sequence
 
